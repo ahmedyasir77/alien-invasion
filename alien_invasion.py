8324592592cbff7a81ec63eaf0794a4342a0860b
Yasir Ahmed <ahmedyasir77@gmail.com>
2021-05-31 18:26:25 -0500

    Added helper functions

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,5 @@
 import sys
-
 import pygame
-
 from settings import Settings
 from ship import Ship 
 
@@ -21,18 +19,32 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
+            self.ship.update()
 
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        # Redraw the screen during each pass through the loop.
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+        
 if __name__ == '__main__':
     # Make a game instance, and run the game. 
     ai = AlienInvasion()
